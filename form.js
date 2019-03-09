887b5e85744d172ae144d650cd34c26ccf45f365
Daphne Liu <daphnel15113455@gmail.com>
2019-03-09 14:55:48 -0800

    Added .gitignore and idb-keyval

---
+++ form.js
@@ -0,0 +1,42 @@
+import { get, set, keys } from "/web_modules/idb-keyval.js";
+
+const form = document.querySelector("form");
+
+form.addEventListener("change", evt => {
+  console.log(evt.target.name, evt.target.value);
+
+  set(evt.target.name, evt.target.value);
+});
+
+async function load() {
+  const inputNames = await keys();
+
+  await Promise.all(
+    inputNames.map(async inputName => {
+      const input = form.elements.namedItem(inputName);
+      if (input != null) {
+        input.value = await get(inputName);
+      }
+    })
+  );
+}
+load();
+
+document.querySelector("#send-email").addEventListener("click", async () => {
+  const [q1 = "", q2 = "", q3 = ""] = await Promise.all([
+    get("resp1"),
+    get("res2"),
+    get("res3")
+  ]);
+
+  const subject = "Forward";
+  const body = `Question 1? ${q1}\nQuestion 2? ${q2}\nQuestion 3? ${q3}`;
+
+  const link = `mailto:?to=&subject=${encodeURIComponent(
+    subject
+  )}&body=${encodeURIComponent(body)}`;
+
+  const anchor = document.createElement("a");
+  anchor.href = link;
+  anchor.click();
+});
