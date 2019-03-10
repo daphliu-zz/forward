4eea137992287ead43d6bb4ef28a710a6d103718
Sarah Bornais <sarah.bornais@gmail.com>
2019-03-10 09:14:03 -0700

    fixed witness form

--- duplicate.js
+++ duplicate.js
@@ -1,6 +1,14 @@
+var count = 0;
+
 function duplicate(div) {
     var original = document.getElementById(div);
     var clone = original.cloneNode(true);
-    clone.id = "";
+    clone.id = "witness" + ++count;
+    var inputs = clone.querySelectorAll('input')
+    for (var x = 0; x < 3; x++) {
+        
+        inputs[x].name += (++count).toString();
+    }
+    
     original.parentNode.appendChild(clone);
 }
